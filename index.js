2dd9e2f4c451ade42edeb877e9f5fc0115ec46bd
Laura-O <laura.ohrndorf@gmail.com>
2018-01-12 16:22:50 +0100

    Basic user registration

--- index.js
+++ index.js
@@ -32,30 +32,27 @@ app.get("/", function(req, res) {
 });
 
 app.get("/petition", function(req, res) {
-    console.log(req.cookies);
+    console.log(req.session.user);
     var scripts = [{ script: "/js/main.js" }];
     res.render("petition-sign", {
         css: "styles.css",
         scripts: scripts,
+        user: {
+            first: req.session.user.first,
+            last: req.session.user.last,
+        },
     });
 });
 
 app.post("/petition", function(req, res) {
-    let query = "INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3) RETURNING id";
+    let query = "INSERT INTO signatures (id, first, last, signature) VALUES ($1, $2, $3, $4)";
 
     pool.connect().then(client => {
         client
-            .query(query, [req.body.first, req.body.last, req.body.hiddensig])
+            .query(query, [req.session.user.id, req.body.first, req.body.last, req.body.hiddensig])
             .then(results => {
-                req.session.sigId = results.rows[0].id;
-                res.cookie("signed", "signed", {
-                    httpOnly: true,
-                });
                 console.log(req.body);
-                res.render("petition-thanks", {
-                    data: req.body,
-                    css: "styles.css",
-                });
+                res.redirect("/thanks");
                 client.release();
             })
             .catch(err => {
@@ -71,23 +68,49 @@ app.get("/register", (req, res) => {
     });
 });
 
+app.post("/register", (req, res) => {
+    console.log(req.body);
+    let query = "INSERT INTO users (first, last, email, pass) VALUES ($1, $2, $3, $4) RETURNING id";
+    pool.connect().then(client => {
+        client
+            .query(query, [req.body.first, req.body.last, req.body.email, req.body.password])
+            .then(results => {
+                req.session.user = {
+                    id: results.rows[0].id,
+                    first: req.body.first,
+                    last: req.body.last,
+                    email: req.body.email,
+                    signed: false,
+                };
+                res.redirect("/petition");
+                client.release();
+            })
+            .catch(err => {
+                client.release();
+                console.error("query error", err.message, err.stack);
+            });
+    });
+});
+
 app.get("/login", (req, res) => {
     res.render("petition-login", {
         css: "styles.css",
     });
 });
 
-app.get("/thanks", requireSignature, (req, res) => {
-    console.log(req.session.sigId);
-
+app.get("/thanks", checkUser, (req, res) => {
     let query = "SELECT * FROM signatures WHERE id=$1";
     pool.connect().then(client => {
         client
-            .query(query, [req.session.sigId])
+            .query(query, [req.session.user.id])
             .then(results => {
                 res.render("petition-thanks", {
                     img: results.rows[0].signature,
                     css: "styles.css",
+                    user: {
+                        first: req.session.user.first,
+                        last: req.session.user.last,
+                    },
                 });
                 client.release();
             })
@@ -138,4 +161,10 @@ function requireSignature(req, res, next) {
     }
 }
 
-function checkAuth(req, res, next) {}
+function checkUser(req, res, next) {
+    if (!req.session.user) {
+        res.reditect("/register");
+    } else {
+        next();
+    }
+}
