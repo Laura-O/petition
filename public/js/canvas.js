86bf0707e880a29dcfa83467d90a92f6dc283146
Laura-O <laura.ohrndorf@gmail.com>
2018-01-14 01:56:42 +0100

    A lot of basic design

---
+++ public/js/canvas.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    const $canvas = $("#canvas");
+    let context = $canvas[0].getContext("2d");
+    let draw = false;
+    let clickX = [];
+    let clickY = [];
+    let clickMove = [];
+
+    $canvas.on("mousedown", function(e) {
+        let mouseX = e.pageX - $canvas.offsetLeft;
+        let mouseY = e.pageY - $canvas.offsetTop;
+
+        draw = true;
+        newStroke(mouseX, mouseY, false);
+        redraw();
+    });
+
+    $canvas.on("mousemove", function(e) {
+        if (draw) {
+            newStroke(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    });
+
+    $canvas.on("mouseup", function(e) {
+        draw = false;
+        redraw();
+    });
+
+    $canvas.on("mouseleave", function(e) {
+        draw = false;
+    });
+
+    function newStroke(x, y, moving) {
+        clickX.push(x);
+        clickY.push(y);
+        clickMove.push(moving);
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, $canvas.width, $canvas.height);
+
+        context.strokeStyle = "black";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (let i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickMove[i]) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+
+    $("#submit").on("click", function(e) {
+        let signature = document.getElementById("canvas").toDataURL();
+        document.getElementById("hiddensig").value = signature;
+    });
+});
--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,18 @@
-$(function() {
+$(document).ready(function() {
     $(".toggleNav").on("click", function() {
         $(".flex-nav ul").toggleClass("open");
     });
+
+    function moveKoopa() {
+        let screenWidth = $(document).width();
+
+        $("#koopa-gif").css("left", screenWidth);
+        $("#koopa-gif").animate({ left: -100 }, 30000, "linear");
+
+        setInterval(function() {
+            moveKoopa();
+        }, 31000);
+    }
+
+    moveKoopa();
 });
